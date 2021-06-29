e18c7dd2b894b424c3f35b777c584401a7ec3103
Staś Ostaszewski <stasostaszewski@gmail.com>
2021-06-29 19:59:54 +0200

    MultiplayerSetUp01

---
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviour
+{
+    public InputField createInput;
+    public InputField joinInput;
+}

93dce32a505237a1b140dd80dff74ce0689e2200
Staś Ostaszewski <stasostaszewski@gmail.com>
2021-06-29 20:05:43 +0200

    MultiplayerConnection

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 
-public class CreateAndJoinRooms : MonoBehaviour
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
     public InputField createInput;
     public InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
 }
